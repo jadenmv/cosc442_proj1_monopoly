ce0d1287355dfb9b336e3c0e696dd29f6b10ab18
jvidall1 <jvidall1@students.towson.edu>
2026-09-20 18:04:37 -0400

    Task 10 - Refactor method getRent() in PropertyCell.java, generate javadocs for PropertyCell.java and Cell.java

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -5,36 +5,71 @@ public abstract class Cell {
 	private String name;
 	protected Player theOwner;
 
+	/**
+	 * Returns the name of this cell.
+	 * @return The name of the cell as a String.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Returns the player who owns this cell.
+	 * @return The Player who owns the cell, or null if none.
+	 */
 	public Player getTheOwner() {
 		return theOwner;
 	}
 	
+	/**
+	 * Returns the price of this cell, defaulting to zero.
+	 * @return The price of the cell as an integer.
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/**
+	 * Checks if this cell is currently available.
+	 * @return True if the cell is available; false otherwise.
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
+	/**
+	 * Executes the specific action associated with this cell.
+	 */
 	public abstract void playAction();
 
+	/**
+	 * Sets the availability status of this cell.
+	 * @param available The new availability status to set.
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/**
+	 * Assigns a name to this cell.
+	 * @param name The name to assign to the cell.
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Assigns an owner to this cell.
+	 * @param owner The Player to set as the owner.
+	 */
 	public void setTheOwner(Player owner) {
 		this.theOwner = owner;
 	}
     
+    /**
+     * Returns the string representation of this cell.
+     * @return The name of the cell as a String.
+     */
     public String toString() {
         return name;
     }
--- src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
@@ -7,22 +7,42 @@ public class PropertyCell extends Cell {
 	private int rent;
 	private int sellPrice;
 
+	/**
+	 * Returns the color group of this property.
+	 * @return The color group as a string.
+	 */
 	public String getColorGroup() {
 		return colorGroup;
 	}
 
+	/**
+	 * Retrieves the price for building a house on this property.
+	 * @return The house price as an integer.
+	 */
 	public int getHousePrice() {
 		return housePrice;
 	}
 
+	/**
+	 * Gets the current number of houses on this property.
+	 * @return The number of houses as an integer.
+	 */
 	public int getNumHouses() {
 		return numHouses;
 	}
     
+    /**
+     * Returns the selling price of this property.
+     * @return The selling price as an integer.
+     */
     public int getPrice() {
 		return sellPrice;
 	}
 
+	/**
+	 * Calculates and returns the rent owed for this property, factoring in houses and monopolies.
+	 * @return The rent amount as an integer.
+	 */
 	public int getRent() {
 		int rentToCharge = rent;
 		String [] monopolies = theOwner.getMonopolies();
@@ -33,6 +53,12 @@ public class PropertyCell extends Cell {
 		return rentToCharge;
 	}
 
+	/**
+	 * Calculates adjusted rent if this property is part of a monopoly group.
+	 * @param rentToCharge The current rent amount to be possibly adjusted.
+	 * @param monopolies Array of color groups that the owner has monopolies in.
+	 * @return The adjusted rent amount as an integer.
+	 */
 	private int calculateMonopoliesRent(int rentToCharge, String[] monopolies) {
 		for(int i = 0; i < monopolies.length; i++) {
 			if(monopolies[i].equals(colorGroup)) {
@@ -42,6 +68,9 @@ public class PropertyCell extends Cell {
 		return rentToCharge;
 	}
 
+	/**
+	 * Executes the action to be taken when a player lands on this property, including rent payment if owned by another player.
+	 */
 	public void playAction() {
 		Player currentPlayer = null;
 		if(!isAvailable()) {
@@ -52,22 +81,42 @@ public class PropertyCell extends Cell {
 		}
 	}
 
+	/**
+	 * Sets the color group of this property.
+	 * @param colorGroup The color group to assign.
+	 */
 	public void setColorGroup(String colorGroup) {
 		this.colorGroup = colorGroup;
 	}
 
+	/**
+	 * Sets the price for building a house on this property.
+	 * @param housePrice The house price to assign.
+	 */
 	public void setHousePrice(int housePrice) {
 		this.housePrice = housePrice;
 	}
 
+	/**
+	 * Sets the number of houses currently on this property.
+	 * @param numHouses The number of houses to set.
+	 */
 	public void setNumHouses(int numHouses) {
 		this.numHouses = numHouses;
 	}
 
+	/**
+	 * Sets the selling price of this property.
+	 * @param sellPrice The selling price to assign.
+	 */
 	public void setPrice(int sellPrice) {
 		this.sellPrice = sellPrice;
 	}
 
+	/**
+	 * Sets the base rent amount for this property.
+	 * @param rent The rent amount to assign.
+	 */
 	public void setRent(int rent) {
 		this.rent = rent;
 	}
